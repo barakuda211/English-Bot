b6d1fa9a37dd76ce267f18e92f4d71f33ee3cba3
barakuda211 <stalkerchnebo@gmail.com>
2019-10-21 22:09:31 +0400

    Бот запускается, но вылетает при получении сообщения

--- English-Bot/EngBot.cs
+++ English-Bot/EngBot.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
 using VkBotFramework;
-using VkNet;
+using VkBotFramework.Models;
+//using VkBotFramework.Examples;
+using VkNet.Model.RequestParams;
+using English_Bot.Properties;
+using static System.Console;
 
 namespace English_Bot
 {
@@ -8,7 +14,36 @@ namespace English_Bot
     {
         static void Main(string[] args)
         {
-            
+            //ILoggerFactory loggerFactory = new LoggerFactory().AddConsole();
+            //ILogger<VkBot> logger = loggerFactory.CreateLogger<VkBot>();
+
+            //ExampleSettings settings = ExampleSettings.TryToLoad(logger);
+
+            VkBot bot = new VkBot(Resources.AccessToken, Resources.groupUrl);
+
+            bot.OnMessageReceived += NewMessageHandler;
+
+            bot.Start();
+
+            WriteLine("Bot started!");
+            ReadLine();
+        }
+        static void NewMessageHandler(object sender, MessageReceivedEventArgs eventArgs)
+        {
+
+            VkBot instanse = sender as VkBot;
+            var peerId = eventArgs.Message.PeerId;
+            var fromId = eventArgs.Message.FromId;
+            var text = eventArgs.Message.Text;
+
+            instanse.Logger.LogInformation($"new message captured. peerId: {peerId},userId: {fromId}, text: {text}");
+            instanse.Api.Messages.Send(new MessagesSendParams()
+            {
+                RandomId = Environment.TickCount,
+                PeerId = eventArgs.Message.PeerId,
+                Message =
+                    $"{fromId.Value}, i have captured your message: '{text}'. its length is {text.Length}. number of spaces: {text.Count(x => x == ' ')}"
+            });
         }
     }
 }
