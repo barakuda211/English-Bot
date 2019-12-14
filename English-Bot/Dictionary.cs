151180696f033b73685ed57fcdd2eccdd5c4852b
barakuda211 <stalkerchnebo@gmail.com>
2019-12-14 16:27:38 +0300

    Убрал обращения к паразитным нэймспейсам

--- English-Bot/Dictionary.cs
+++ English-Bot/Dictionary.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Project_Word;
-using System.Linq;
-
+﻿using System.Collections.Generic;
+//using Project_Word;
 
 namespace English_Bot
 {
--- English-Bot/Users.cs
+++ English-Bot/Users.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Project;
 
 namespace English_Bot 
 {
