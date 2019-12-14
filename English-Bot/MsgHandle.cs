8b55c8380ab0ad77cfe40640a65a07f5f9ce4420
barakuda211 <stalkerchnebo@gmail.com>
2019-12-14 17:23:22 +0300

    Создал перебор вариантов ответа бота в зависимости от сообщения

--- English-Bot/MsgHandle.cs
+++ English-Bot/MsgHandle.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using VkBotFramework;
 using VkBotFramework.Models;
 using VkNet.Model.RequestParams;
+using VkNet.Model;
 using English_Bot.Properties;
 using static System.Console;
 
@@ -13,20 +14,26 @@ namespace English_Bot
     {
         static void NewMessageHandler(object sender, MessageReceivedEventArgs eventArgs)
         {
-
             VkBot instanse = sender as VkBot;
             var peerId = eventArgs.Message.PeerId;
             var fromId = eventArgs.Message.FromId;
             var text = eventArgs.Message.Text;
-   
+            var answer = "Sorry, there is a empty answer :-(";
+
+            switch (text)
+            {
+                default: answer = SendInfo(eventArgs.Message);
+                        break;
+            }
             instanse.Logger.LogInformation($"new message captured. peerId: {peerId},userId: {fromId}, text: {text}");
             instanse.Api.Messages.Send(new MessagesSendParams()
             {
                 RandomId = Environment.TickCount,
                 PeerId = eventArgs.Message.PeerId,
-                Message =
-                    $"{fromId.Value}, i have captured your message: '{text}'. its length is {text.Length}. number of spaces: {text.Count(x => x == ' ')}"
+                Message = answer
             });
         }
+
+        static string SendInfo(Message msg) => $"{msg.PeerId.Value}, i have captured your message: '{msg.Text}'. its length is {msg.Text.Length}. number of spaces: {msg.Text.Count(x => x == ' ')}";
     }
 }
