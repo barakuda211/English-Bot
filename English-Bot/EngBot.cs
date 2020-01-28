7d8a050a4e1d658ec697c1f5d6ccb35b73a6b4f2
mixae1 <56720762+mixae1@users.noreply.github.com>
2020-01-28 15:13:24 +0300

    Added метод ожидания слова от пользователя Added метод тестирования Added в основную программу создание dictionary, users Added вспомогательная переменная (word, userID, used) с возможной заменой на массив (word, userID) из необработанных слов ботом

--- English-Bot/EngBot.cs
+++ English-Bot/EngBot.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using VkBotFramework;
 using VkBotFramework.Models;
 //using VkBotFramework.Examples;
 using VkNet.Model.RequestParams;
 using English_Bot.Properties;
 using static System.Console;
+using Project;
+using Project_Word;
 
 namespace English_Bot
 {
     class EngBot
     {
+        static Dictionary dictionary = new Dictionary();//подгрузку из файла нужно сделать
+        static Users users = new Users();//подгрузку из файла нужно сделать(или из Resources)
         static VkBot bot = new VkBot(Resources.AccessToken, Resources.groupUrl);
+        static (long, string) userMessage;
         static void Main(string[] args)
         {
             //ILoggerFactory loggerFactory = new LoggerFactory().AddConsole();
@@ -20,7 +26,8 @@ namespace English_Bot
 
             //ExampleSettings settings = ExampleSettings.TryToLoad(logger);
 
-            SendMessage(210036813, "hi");
+            users.AddUser(new User(210036813, 1, null, null, null));
+            Testing(210036813);
 
             bot.OnMessageReceived += NewMessageHandler;
 
@@ -37,6 +44,9 @@ namespace English_Bot
             var fromId = eventArgs.Message.FromId;
             var text = eventArgs.Message.Text;
 
+            userMessage.Item1 = fromId.Value;
+            userMessage.Item2 = text;
+
             instanse.Logger.LogInformation($"new message captured. peerId: {peerId},userId: {fromId}, text: {text}");
             instanse.Api.Messages.Send(new MessagesSendParams()
             {
@@ -46,6 +56,7 @@ namespace English_Bot
                     $"{fromId.Value}, i have captured your message: '{text}'. its length is {text.Length}. number of spaces: {text.Count(x => x == ' ')}"
             });
         }
+        //отправляет сообщение юзеру
         static void SendMessage(long userID, string message)
         {
             bot.Api.Messages.Send(new MessagesSendParams()
@@ -54,6 +65,47 @@ namespace English_Bot
                 Message = message,
                 RandomId = Environment.TickCount
             });
+        }
+        //ждет ответа !определенного! ответа от юзра, 
+        //always отвечает за время ожидания(false - 1 попытка, true - ждет, пока юзер не напишет нужное)
+        static bool WaitWordFromUser(long userID, string word, bool always)
+        {
+            if (always) while (userMessage.Item1 != userID && userMessage.Item2.ToLower() != word) ;  //ожидание согласия(нужно использовать поток)   
+            if (!always)
+            { 
+                while (userMessage.Item1 != userID) ;
+                return userMessage.Item2.ToLower() == word;
+            }
+            return true;//заглушка
+        }
+        //тестирование пользователя по !6! последним изученным словам
+        static void Testing(long userID)
+        {
+            SendMessage(userID, "Nu 4to, hlop4ik, sigraem v igru? Pishi \"Готов\"");
+            WaitWordFromUser(userID, "готов", true);          
+
+            //если слов меньше, то так тому и быть
+            var lastLW = users.GetUser(userID).learnedWords.TakeLast(6);
+
+            var rand = new Random();
+
+            //лист для проверки ответов
+            List<bool> right = new List<bool>(6);
+            int cnt = 0;
+
+            foreach(long idx in lastLW)
+            {
+                var word = dictionary.GetWord(idx);
+                int r = rand.Next(2);
+                SendMessage(userID, r == 0 ? word.eng : word.rus);
+                right[cnt] = WaitWordFromUser(userID, r == 1 ? word.eng : word.rus, false);
+                cnt++;
+            }
+
+            SendMessage(userID, $"Вы ответили на {right.FindAll(x => x).Count()} из {lastLW.Count()}. Good job!(no)");
+            
+
+            //исправление ошибок юзера
         }
     }
 }
