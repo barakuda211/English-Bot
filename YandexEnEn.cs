0baf052b2c4deb53f7ee2ca73e62a2f3b3679290
Steve <36530954+Outrociaocul@users.noreply.github.com>
2020-01-28 18:46:14 +0300

    Классы яндекс словаря

---
+++ YandexEnEn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace SpaceYandexEnEn
+{
+    [DataContract]
+    public class Head
+    {
+    }
+
+    [DataContract]
+    public class Syn
+    {
+        [DataMember]
+        public string text { get; set; }
+        [DataMember]
+        public string pos { get; set; }
+    }
+
+    [DataContract]
+    public class Tr
+    {
+        [DataMember]
+        public string pos { get; set; }
+        [DataMember]
+        public List<Syn> syn { get; set; }
+        [DataMember]
+        public string text { get; set; }
+    }
+
+    [DataContract]
+    public class Def
+    {
+        [DataMember]
+        public string text { get; set; }
+        [DataMember]
+        public string pos { get; set; }
+        [DataMember]
+        public string ts { get; set; }
+        [DataMember]
+        public List<Tr> tr { get; set; }
+    }
+
+    [DataContract]
+    public class YandexEnEn
+    {
+        [DataMember]
+        public Head head { get; set; }
+        [DataMember]
+        public List<Def> def { get; set; }
+    }
+}
---
+++ YandexRuEn.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceYandexRuEn
+{
+    public class Head
+    {
+    }
+
+    public class Mean
+    {
+        public string text { get; set; }
+    }
+
+    public class Tr2
+    {
+        public string text { get; set; }
+    }
+
+    public class Ex
+    {
+        public string text { get; set; }
+        public List<Tr2> tr { get; set; }
+    }
+
+    public class Syn
+    {
+        public string text { get; set; }
+        public string pos { get; set; }
+    }
+
+    public class Tr
+    {
+        public string text { get; set; }
+        public string pos { get; set; }
+        public List<Mean> mean { get; set; }
+        public List<Ex> ex { get; set; }
+        public List<Syn> syn { get; set; }
+    }
+
+    public class Def
+    {
+        public string text { get; set; }
+        public string pos { get; set; }
+        public string gen { get; set; }
+        public string anm { get; set; }
+        public List<Tr> tr { get; set; }
+    }
+
+    public class YandexRuEn
+    {
+        public Head head { get; set; }
+        public List<Def> def { get; set; }
+    }
+}
