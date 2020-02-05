472357ce0771f16403e7d29b433cb78478fba0f6
Gleb Mostovoy <nikibook@bk.ru>
2020-02-05 22:00:26 +0300

    Вернул Methods

--- English-Bot/Dictionary.cs
+++ English-Bot/Dictionary.cs
@@ -6,11 +6,24 @@ namespace English_Bot
     public class Dictionary
     {
         private Dictionary<long, Word> dict;
-      
+        private Dictionary<string, long[]> eng_ids; 
+        private Dictionary<string, long[]> rus_ids; 
         
         public Dictionary()
         {
             dict = new Dictionary<long, Word>();
+            foreach (var word in Methods.Deserialization<Word>(@"Json dicts/eng_words.json"))
+            {
+                dict.Add(word.id, word);
+                if (eng_ids.ContainsKey(word.eng))
+                    eng_ids[word.eng].Add(word.id);
+                else 
+                    eng_ids.Add(word.eng, word.id);
+                if (rus_ids.ContainsKey(word.rus))
+                    rus_ids[word.rus].Add(word.id);
+                else
+                    rus_ids.Add(word.rus, word.id);
+            }
         }
         /// <summary>
         /// индексация с 1
@@ -30,42 +43,21 @@ namespace English_Bot
                 return dict[id];
             else
                 return null;
+        }
 
+        /// <summary>
+        /// получаем id английских слова  по нашему запросу  
+        /// </summary>
+        public long[] GetWordEng(string word)
+        {
+            return eng_ids[word];
         }
         /// <summary>
-        /// получаем английские слова  по нашему запросу  
+        /// получаем id русских слова  по нашему запросу  
         /// </summary>
-        public Word[] GetWordEng(string word)
+        public long[] GetWordRus(string word)
         {
-            //работает ,но вроде как такие запросы выполняются медленнее чем обычный цикл
-            // var lst = dict.Select(t=>t.Value).Where(d => d.eng.Contains(word));
-
-            List<Word> lst = new List<Word>();
-            
-            foreach (var t in dict)
-            {
-                if (t.Value.eng.Contains(word.ToLower()))
-                    lst.Add(t.Value);
-            }
-            
-            return lst.ToArray();
-        }
-        /// <summary>
-        /// получаем русские слова  по нашему запросу  
-        /// </summary>
-        public Word[] GetWordRus(string word)
-        {
-
-            ////работает ,но вроде как такие запросы выполняются медленнее чем обычный цикл
-            //var lst = dict.Select(t => t.Value).Where(d => d.rus.Contains(word));
-            List<Word> lst = new List<Word>();
-
-            foreach (var t in dict)
-            {
-                if (t.Value.rus.Contains(word.ToLower()))
-                    lst.Add(t.Value);
-            }
-            return lst.ToArray();
+            return rus_ids[word];
         }
 
         public bool AddWord(Word w)
---
+++ English-Bot/Methods.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+using System.Net; 
+
+namespace Dictionary
+{
+    /// <summary>
+    /// Используем вебреквест
+    /// </summary>
+    public class TimedWebClient : WebClient
+    {
+        // Timeout in milliseconds, default = 600,000 msec
+        public int Timeout { get; set; }
+
+        public TimedWebClient()
+        {
+            this.Timeout = 600000;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var objWebRequest = base.GetWebRequest(address);
+            objWebRequest.Timeout = this.Timeout;
+            return objWebRequest;
+        }
+    }
+
+    class Methods
+    {
+        /// <summary>
+        /// Исправление кодировки
+        /// </summary>
+        public static void ChangeEncoding(ref string response)
+        {
+            Encoding fromEncodind = Encoding.GetEncoding(1251); //из какой кодировки
+            byte[] bytes = fromEncodind.GetBytes(response);
+            Encoding toEncoding = Encoding.UTF8; //в какую кодировку
+            response = toEncoding.GetString(bytes);
+        }
+
+        /// <summary>
+        /// Выполнение запроса и запись в файл по указанному пути -- устарело
+        /// </summary>
+        public static string Request(string url, string fName)
+        {
+            return "Сигнатура устарела";
+        }
+        /// <summary>
+        /// Выполнение запроса и (запись в файл по указанному пути -- устарело) возврат строки-ответа
+        /// </summary>
+        public static string Request(string url)
+        {
+            // Выполняем запрос по адресу и получаем ответ в виде строки (Используем вебреквест!)
+            try
+            {
+                string response = new TimedWebClient { Timeout = 1000 }.DownloadString(url);
+
+                // Исправляем кодировку
+                ChangeEncoding(ref response);
+                // Пишем во временный файл -- устарело
+                // File.WriteAllText(prefix + fName + ".json", response);
+
+                // Возвращаем строку-ответ (формат JSON)
+                return response;
+            }
+            catch (System.Net.WebException ex)
+            {
+                return '!' + ex.Message;
+            }
+
+            /*// Создаём объект WebClient
+            using (WebClient webClient = new WebClient())
+            {
+                // Выполняем запрос по адресу и получаем ответ в виде строки
+                string response = webClient.DownloadString(url);
+                // Исправляем кодировку
+                ChangeEncoding(ref response);
+                // Пишем во временный файл
+                File.WriteAllText(prefix + fName + ".json", response);
+            }*/
+        }
+
+        /// <summary>
+        /// Десериализация в массив объектов -- устарело
+        /// </summary>
+        public static T[] DeSerialization<T>(string fName)
+        {
+            T[] objs;
+            //Десериализуем файл
+            using (FileStream fs = new FileStream(fName + ".json", FileMode.OpenOrCreate))
+            {
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(T[]));
+                objs = (T[])jsonFormatter.ReadObject(fs);
+            }
+            return objs;
+        }
+
+        /// <summary>
+        /// Десериализация в объект -- устарело
+        /// </summary>
+        public static T DeSerializationObj<T>(string fName)
+        {
+            // Объект, в который будет произведена десериализация
+            T obj;
+            //Десериализуем файл
+            using (FileStream fs = new FileStream(fName + ".json", FileMode.OpenOrCreate))
+            {
+                DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(T));
+                obj = (T)jsonFormatter.ReadObject(fs);
+            }
+            return obj;
+        }
+
+        /// <summary>
+        /// Десериализация в объект по строке
+        /// </summary>
+        public static T DeSerializationObjFromStr<T>(string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+
+        /// <summary>
+        /// Десериализация в массив объектов по строке
+        /// </summary>
+        public static T[] DeSerializationFromStr<T>(string str)
+        {
+            return JsonConvert.DeserializeObject<T[]>(str);
+        }
+    }
+}
