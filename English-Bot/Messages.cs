071ea91aaa7e7ce2ddfe78910386eb9aef4ea749
Gleb Mostovoy <nikibook@bk.ru>
2020-04-02 17:31:31 +0300

    Пофиксил, добавил проверки

--- English-Bot/Messages.cs
+++ English-Bot/Messages.cs
@@ -9,7 +9,9 @@ using System.Text;
 using System.Linq;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
-using NAudio; 
+using NAudio;
+using Alvas.Audio;
+using System.IO;
 
 namespace English_Bot
 {
@@ -23,34 +25,43 @@ namespace English_Bot
         {
             string message = "";
             Word word = dictionary[wordId];
-            if (word.mean_eng != null && word.mean_eng.def != null)
-                message += word.eng.ToUpper() + " [" + word.mean_eng.def[0].ts + "]\n";
-            message += "Определения:\n";
-            bool exps = false;
-            foreach (var def in word.mean_rus.def)
+            try
             {
-                message += "-----------" + def.text + " " + def.pos + ".:\n";
-                //string syns = "";
-                foreach (var tr in def.tr)
+                if (word.mean_eng != null && word.mean_eng.def != null)
+                    message += word.eng.ToUpper() + " [" + ((word.tags != null && word.tags.Contains("eng_only")) ? word.mean_eng.def[0].ts : word.mean_rus.def[0].ts) + "]\n";
+                message += "Определения:\n";
+                bool exps = false;
+                foreach (var def in word.mean_rus.def)
                 {
-                    //syns += "Синонимы:\n";
-                    message += "-> " + tr.text + /*" (" + dictionary[dictionary.GetRusWordIds(tr.text)[0]]?.eng + ")" + */ "\n";
-                    if (tr.syn != null)
+                    message += "-----------" + def.text + " " + def.pos + ".:\n";
+                    //string syns = "";
+                    foreach (var tr in def.tr)
                     {
-                        //foreach (var syn in tr.syn)
+                        //syns += "Синонимы:\n";
+                        message += "-> " + tr.text + /*" (" + dictionary[dictionary.GetRusWordIds(tr.text)[0]]?.eng + ")" + */ "\n";
+                        if (tr.syn != null)
+                        {
+                            //foreach (var syn in tr.syn)
                             //syns += tr.syn[0].text + ", ";
-                        //syns = syns.Substring(0, syns.Length - 2);
+                            //syns = syns.Substring(0, syns.Length - 2);
+                        }
+                    }
+                    if (!exps && def.tr[0].ex != null)
+                    {
+                        message += "Примеры: \n";
+                        foreach (var ex in def.tr[0].ex)
+                            message += ex.text + " - " + ex.tr[0].text + "\n";
+                        exps = true;
                     }
                 }
-                if (!exps && def.tr[0].ex != null)
-                {
-                    message += "Примеры: \n";
-                    foreach (var ex in def.tr[0].ex)
-                        message += ex.text + " - " + ex.tr[0].text + "\n";
-                    exps = true;
-                }
             }
-            SendMessage(userId, message);
+            catch (Exception e)
+            {
+                Console.WriteLine("Отправка описания слова неудачна: ID = " + userId + ", Word id = " + wordId);
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            SendMessage(userId, message == "" ? "Ошибка" : message);
             //return message; 
         } 
 
@@ -60,13 +71,30 @@ namespace English_Bot
             string an = "Я не знаю такого слова :(";
             if (word[0] >= 'A' && word[0] <= 'z')
             {
-                return !dictionary.eng_ids.ContainsKey(word) ? an : string.Join('/', from def in dictionary[dictionary.eng_ids[word]].mean_rus.def select def.tr[0].text);
+                try
+                {
+                    return !dictionary.eng_ids.ContainsKey(word) ? an : string.Join('/', from def in dictionary[dictionary.eng_ids[word]].mean_rus.def select def.tr[0].text);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
             }
             else
             {
-                var list = dictionary.rus_ids.ContainsKey(word) ? dictionary.rus_ids[word] : null;
-                return (list == null || list.Count == 0) ? an : string.Join('/', list.Select(x => dictionary[x]?.eng));
+                try
+                {
+                    var list = dictionary.rus_ids.ContainsKey(word) ? dictionary.rus_ids[word] : null;
+                    return (list == null || list.Count == 0) ? an : string.Join('/', list.Select(x => dictionary[x]?.eng));
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
             }
+            return an; 
         }
 
         static void SendPicture(long id, long word)
@@ -90,13 +118,16 @@ namespace English_Bot
                 int width = pics.hits[0].webformatWidth;
                 int height = pics.hits[0].webformatHeight;
                 int font_size = 36; // 3 * (width / 50) - 4;
-                int tr_size = 32; // 2 * (width / 100); 
-                graphImage.DrawString(text, new Font(FontFamily.Families[font].Name, font_size, FontStyle.Regular), new SolidBrush(ColorTranslator.FromHtml("#000000")), new Point(width / 2 - (int)((text.Length / (double)2) * font_size * 2), height / 2 - (height / 4)), new StringFormat(StringFormatFlags.NoClip));
-                text = "[" + dictionary[word].mean_eng.def[0].ts + "]";
-                graphImage.DrawString(@text, new Font(FontFamily.Families[font].Name, font_size, FontStyle.Regular), new SolidBrush(ColorTranslator.FromHtml("#000000")), new Point(width / 2 - (int)((text.Length / (double)2) * font_size * 2), height / 2 + 50), new StringFormat(StringFormatFlags.NoClip));
-                text = string.Join('/', dictionary[word].mean_rus.def.Select(x => x.tr[0].text));
-                graphImage.DrawString(text, new Font(FontFamily.Families[font].Name, tr_size, FontStyle.Regular), new SolidBrush(ColorTranslator.FromHtml("#000000")), new Point(width / 2 - (int)((text.Length / (double)2) * font_size) + 10, height / 2 + 25*(height / 100)), new StringFormat(StringFormatFlags.NoClip));
-                bitmap.Save(word + "_picture_with_str.jpg");
+                int tr_size = 24; // 2 * (width / 100); 
+                graphImage.DrawString(text, new Font(FontFamily.Families[font].Name, font_size, FontStyle.Regular), new SolidBrush(ColorTranslator.FromHtml("#000000")), new Point(width / 2 - (int)((text.Length / (double)2) * (font_size + 20) * 2), height / 2 - (height / 4)), new StringFormat(StringFormatFlags.NoClip));
+                text = "[" + ((dictionary[word].tags != null && dictionary[word].tags.Contains("eng_only")) ? dictionary[word].mean_eng.def[0].ts : dictionary[word].mean_rus.def[0].ts) + "]";
+                graphImage.DrawString(@text, new Font(FontFamily.Families[font].Name, tr_size, FontStyle.Regular), new SolidBrush(ColorTranslator.FromHtml("#000000")), new Point(width / 2 - (int)((text.Length / (double)2) * font_size * 2), height / 2 + 50), new StringFormat(StringFormatFlags.NoClip));
+                if (!(dictionary[word].tags != null && dictionary[word].tags.Contains("eng_only")))
+                {
+                    text = string.Join('/', dictionary[word].mean_rus.def.Select(x => x.tr[0].text));
+                    graphImage.DrawString(text, new Font(FontFamily.Families[font].Name, font_size, FontStyle.Regular), new SolidBrush(ColorTranslator.FromHtml("#000000")), new Point(width / 2 - (int)((text.Length / (double)2) * (font_size + 20) * 2), height / 2 + 25 * (height / 100)), new StringFormat(StringFormatFlags.NoClip));
+                }
+                    bitmap.Save(word + "_picture_with_str.jpg");
 
                 // System.Threading.Thread.Sleep(100); 
 
@@ -112,19 +143,23 @@ namespace English_Bot
                     Attachments = photos
                 }) ;
             }
-            catch (Exception)
-            { return; }
+            catch (Exception e)
+            { 
+                Console.WriteLine("Отправка фото неудачна: ID = " + id + ", Word id = " + word);
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
-        /*
-        static void ToWav()
+
+        
+        static void ToWav(string file)
         {
-            string fileName = @"e:\down\male.ogg";
-            using (DsReader dr = new DsReader(fileName))
+            using (DsReader dr = new DsReader(file))
             {
                 if (dr.HasAudio)
                 {
                     IntPtr format = dr.ReadFormat();
-                    using (WaveWriter ww = new WaveWriter(File.Create(fileName + ".wav"),
+                    using (WaveWriter ww = new WaveWriter(File.Create(file + ".wav"),
                         AudioCompressionManager.FormatBytes(format)))
                     {
                         byte[] data = dr.ReadData();
@@ -134,25 +169,13 @@ namespace English_Bot
             }
         }
 
-        static void OggToWavWithOgg()
+        /*
+        static void ToOgg(string file)
         {
-            string fileName = @"e:\Down\fortuna.ogg";
-            using (DsReader dr = new DsReader(fileName))
-            {
-                if (dr.HasAudio)
-                {
-                    short oggFormatTag = 26479;
-                    IntPtr format = dr.ReadFormat();
-                    WaveFormat waveFormat = AudioCompressionManager.GetWaveFormat(format);
-                    IntPtr formatOgg = AudioCompressionManager.GetCompatibleFormat(format, oggFormatTag);
-                    using (WaveWriter ww = new WaveWriter(File.Create(fileName + ".wav"),
-                        AudioCompressionManager.FormatBytes(formatOgg)))
-                    {
-                        AudioCompressionManager.Convert(dr, ww, true);
-                    }
-                }
-            }
-            */
+            Sox.Convert(@"c:\Program Files (x86)\sox-14-4-1\sox.exe", file, file + ".ogg");
+        }
+        */
+
         static void SendSound(long id, long word)
         {
             SpeechSynthesizer speechSynth = new SpeechSynthesizer();
@@ -167,6 +190,12 @@ namespace English_Bot
             {
                 // :3
             }
+        } 
+
+        static void SendRecognition(EventArgs message)
+        {
+            SpeechRecognizer rec = new SpeechRecognizer(); 
+            
         }
         
     }
