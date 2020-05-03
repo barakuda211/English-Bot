30b9acc370c658d38cf1155c8526603d23cc14f2
barakuda211 <stalkerchnebo@gmail.com>
2020-05-03 18:42:42 +0300

    Смена токена

--- English-Bot/EngBot.cs
+++ English-Bot/EngBot.cs
@@ -22,8 +22,8 @@ namespace English_Bot
         //public const string Token = "41df7d2e30314de0f847a51c4f8beaaf8d287eda3e31527d44a3d7aa17dac4928b9dc16be8ee476c64916";
 
         //менять только для смены паблика
-        public static string Token = Resources.AccessToken;
-        public static string Url = Resources.groupUrl;
+        public static string Token = Resources.AccessToken2;
+        public static string Url = Resources.groupUrl2;
 
         public static WordsDictionary dictionary = new WordsDictionary();
         public static Users users = new Users();//подгрузку из файла нужно сделать(или из Resources)
